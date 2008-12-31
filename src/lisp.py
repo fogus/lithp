f49a4b688fecb05d501376f7f49797508fc5f4dd
fogus <mefogus@gmail.com>
2008-12-30 20:08:38 -0500

    some commentary

--- src/lisp.py
+++ src/lisp.py
@@ -1,3 +1,15 @@
 class Lisp:
+    """
+    The original Lisp described by McCarthy in his 1960 paper describes the following function set:
+    1.  `atom`
+    2.  `car`
+    3.  `cdr`
+    4.  `cond`
+    5.  `cons`
+    6.  `eq`
+    7.  `quote`
+
+    http://www-formal.stanford.edu/jmc/recursive.html
+    """
     def dummy(self, env, args):
         print "I do nothing"
