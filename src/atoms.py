d6ade559e75e36923f7854f9818ce05d0f7ba4c7
Fogus <mefogus@gmail.com>
2009-01-30 15:55:32 -0500

    rearranging symbols

---
+++ src/atoms.py
@@ -0,0 +1,16 @@
+from interface import Eval
+
+class Symbol(Eval):
+    def __init__(self, n):
+        self.name = n
+
+    def __repr__(self):
+        return self.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def eval(self, env, args=None):
+        return env.get(self.name)
+
+    
