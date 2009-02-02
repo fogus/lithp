50eb8b3317acbec299b5586e339aef9011159415
Fogus <mefogus@gmail.com>
2009-02-02 11:16:36 -0500

    Basic scanner for integers

--- src/scanner.py
+++ src/scanner.py
@@ -1,3 +1,6 @@
+import string
+import re
+
 from atom import Symbol
 from atom import String
 from number import Number
@@ -5,7 +8,7 @@ from number import Number
 class Scanner:
     def __init__(self, str=None):
        print("Initializing the scanner...")
-       self.raw_source
+       self.raw_source = str
        self.index = 0
        self.length = 0
        self.sexpr = []
@@ -19,7 +22,30 @@ class Scanner:
             self.length = len(self.raw_source)
             self.index = 0
 
-        
+        return self.get_token()
 
     def get_token(self):
+        if self.index >= self.length:
+            return None
+
+        # Kill whitespace
+        while self.index < self.length and self.raw_source[self.index] in string.whitespace:
+            self.index = self.index + 1
+
+        # Check if we had a string of whitespace
+        if self.index == self.length:
+            return None       
+
+        token_str = ""
         
+        # Build the token string
+        while self.index < self.length - 1:
+            token_str = token_str + self.raw_source[self.index]
+            self.index = self.index + 1
+
+        int_regex = re.compile(r'^[+-]?\d+$')
+        
+        if int_regex.match(token_str):
+            return Number(string.atoi(token_str))
+
+        return None
