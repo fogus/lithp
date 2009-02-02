a8a88f5c10f15f0950f76cf3bebadcd3c07e3212
Fogus <mefogus@gmail.com>
2009-02-02 10:59:30 -0500

    Tokenize stub

--- src/scanner.py
+++ src/scanner.py
@@ -5,6 +5,13 @@ from number import Number
 class Scanner:
     def __init__(self, str=None):
        print("Initializing the scanner...")
+       self.raw_source
+       self.index = 0
+       self.length = 0
+       self.sexpr = []
 
     def get_sexpr(self, source=None):
         return source
+
+    def get_token(self):
+        
