50e612aec27897abcf1fd20a90ba9966eda93de6
Fogus <mefogus@gmail.com>
2009-02-03 16:03:59 -0500

    Fixed println spelling error

--- src/lisp.py
+++ src/lisp.py
@@ -15,12 +15,12 @@ class Lisp:
     """
     def dummy(self, env, args):
         print("I do nothing, but you gave me: ")
-        self.prinln(env, args)
+        self.println(env, args)
 
-    def println( self, env, args):
+    def println(self, env, args):
         for a in args:
             result = a.eval(env)
             self.stdout.write( "%s " % str( result))
-            
-        self.stdout.write( "\n")    
+
+        self.stdout.write( "\n")
         return TRUE
