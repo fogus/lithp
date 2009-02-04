4b641cb9a551199c30aa074cc4f1616cbb8cf00c
Fogus <mefogus@gmail.com>
2009-02-04 10:05:04 -0500

    Different repr for functions

--- src/function.py
+++ src/function.py
@@ -4,5 +4,8 @@ class Function(Eval):
     def __init__(self, fn):
         self.fn = fn
 
+    def __repr__( self):
+        return "<built-in function %s>" % id( self.fn)
+
     def eval(self, env, args):
         return self.fn(env, args)
