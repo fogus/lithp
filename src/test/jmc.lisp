3389b124c16a471a7c7932aa390e361fdb75ee87
Fogus <mefogus@gmail.com>
2009-02-23 13:05:38 -0500

    Almost homoiconic

--- src/test/jmc.lisp
+++ src/test/jmc.lisp
@@ -34,3 +34,28 @@
 (label cadar (lambda (x) (car (cdr (car x)))))
 (label caddar (lambda (x) (car (cdr (cdr (car x))))))
 
+(label eval (lambda (e a)
+  (cond
+    ((atom e) (assoc e a))
+    ((atom (car e))
+     (cond
+       ((eq (car e) (quote quote)) (cadr e))
+       ((eq (car e) (quote atom))  (atom   (eval (cadr e) a)))
+       ((eq (car e) (quote eq))    (eq     (eval (cadr e) a)
+                                    (eval (caddr e) a)))
+       ((eq (car e) (quote car))   (car    (eval (cadr e) a)))
+       ((eq (car e) (quote cdr))   (cdr    (eval (cadr e) a)))
+       ((eq (car e) (quote cons))  (cons   (eval (cadr e) a)
+                                    (eval (caddr e) a)))
+       ((eq (car e) (quote cond))  (evcon (cdr e) a))
+       (t (eval (cons (assoc (car e) a)
+                        (cdr e))
+                  a))))
+    ((eq (caar e) (quote label))
+     (eval (cons (caddar e) (cdr e))
+            (cons (list (cadar e) (car e)) a)))
+    ((eq (caar e) (quote lambda))
+     (eval (caddar e)
+            (append. (pair (cadar e) (evlis (cdr e) a))
+                     a))))))
+
