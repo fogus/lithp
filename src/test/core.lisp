e3ce2abea07c49f3171eee989f20f77db73b4111
Fogus <mefogus@gmail.com>
2009-03-31 21:45:08 -0400

    filter function

--- src/test/core.lisp
+++ src/test/core.lisp
@@ -15,3 +15,10 @@
                (t (f (car lst)
                      (reduce f (cdr lst)))))))
 
+(def filter (lambda (f lst)
+              (cond
+               ((null lst) lst)
+               (t (cond
+                   ((f (car lst)) (cons (car lst) (filter f (cdr lst))))
+                   (t (filter f (cdr lst))))))))
+
