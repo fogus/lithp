a21dbf7df8b1c02bea6f990bad9b92211a2f6840
Fogus <mefogus@gmail.com>
2009-04-15 21:17:43 -0400

    Changing var names to avoid dynamic scope clashing

--- src/test/core.lisp
+++ src/test/core.lisp
@@ -1,33 +1,34 @@
-(def and (lambda (x y)
-           (cond (x
-                  (cond (y t)
+(def and (lambda (and_x and_y)
+           (cond (and_x
+                  (cond (and_y t)
                         (t nil)))
                  (t nil))))
 
-(def not (lambda (x)
-           (cond (x nil)
+(def not (lambda (not_x)
+           (cond (not_x nil)
                  (t t))))
 
-(def nand (lambda (x y)
-            (not (and x y))))
+(def nand (lambda (nand_x nand_y)
+            (not (and nand_x nand_y))))
 
-(def or (lambda (x y)
+(def or (lambda (or_x or_y)
           (nand
-           (nand x x) 
-           (nand y y))))
+           (nand or_x or_x)
+           (nand or_y or_y))))
 
-(def nor (lambda (x y)
-           (not (or x y))))
+(def nor (lambda (nor_x nor_y)
+           (not (or nor_x nor_y))))
 
-(def xor (lambda (x y)
-           (or 
-            (and x (not y)) 
-            (and (not x) y))))
+(def xor (lambda (xor_x xor_y)
+           (or
+            (and xor_x (not xor_y))
+            (and (not xor_x) xor_y))))
 
-(def xr (lambda (x y)
-          (nand
-           (nand x (nand x y))
-           (nand y (nand x y)))))
+(def and_ (lambda (x y) (cond (x (cond (y t) (t nil))) (t nil))))
+(def not_ (lambda (x) (cond (x nil) (t t))))
+(def nand_ (lambda (x y) (not_ (and_ x y))))
+(def or_ (lambda (x y) (nand_ (nand_ x x) (nand_ y y))))
+(def xor_ (lambda (x y) (or_ (and_ x (not_ y)) (and_ (not_ x) y))))
 
 (def mapcar (lambda (f lst)
               (cond
