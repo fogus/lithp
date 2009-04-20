21dd53082651175adc429539e1936abf28723e6a
Fogus <mefogus@gmail.com>
2009-04-20 12:48:16 -0400

    Added Paul Graham's eval

--- src/test/core.lisp
+++ src/test/core.lisp
@@ -37,9 +37,8 @@
                    (mapcar_f (car mapcar_lst))
                    (mapcar mapcar_f (cdr mapcar_lst))))))))
 
-
 (def null (lambda (null_x)
-              (eq null_x nil)))
+            (eq null_x nil)))
 
 (def reduce (lambda (reduce_f reduce_lst)
               (cond
@@ -61,4 +60,56 @@
                                                  (filter filter_f (cdr filter_lst))))
                    (t (filter filter_f (cdr filter_lst))))))))
 
+(def list (lambda (x y)
+            (cons x (cons y (quote ())))))
 
+(def pair (lambda (x y)
+            (cond ((and (null x) (null y)) (quote ()))
+                  ((and (not (atom x)) (not (atom y)))
+                   (cons (list (car x) (car y))
+                         (pair (cdr x) (cdr y)))))))
+
+(def assoc (lambda (x y)
+             (cond ((eq (car (car y)) x) (car (cdr (car y))))
+                   (t (assoc x (cdr y))))))
+
+(def caar (lambda (x) (car (car x))))
+(def cadr (lambda (x) (car (cdr x))))
+(def caddr (lambda (x) (car (cdr (cdr x)))))
+(def cadar (lambda (x) (car (cdr (car x)))))
+(def caddar (lambda (x) (car (cdr (cdr (car x))))))
+
+(def eval (lambda (e a)
+            (cond
+             ((atom e) (assoc e a))
+             ((atom (car e))
+              (cond
+               ((eq (car e) (quote quote)) (cadr e))
+               ((eq (car e) (quote atom))  (atom   (eval (cadr e) a)))
+               ((eq (car e) (quote eq))    (eq     (eval (cadr e) a)
+                                                   (eval (caddr e) a)))
+               ((eq (car e) (quote car))   (car    (eval (cadr e) a)))
+               ((eq (car e) (quote cdr))   (cdr    (eval (cadr e) a)))
+               ((eq (car e) (quote cons))  (cons   (eval (cadr e) a)
+                                                   (eval (caddr e) a)))
+               ((eq (car e) (quote cond))  (evcon (cdr e) a))
+               (t (eval (cons (assoc (car e) a)
+                              (cdr e))
+                        a))))
+             ((eq (caar e) (quote def))
+              (eval (cons (caddar e) (cdr e))
+                    (cons (list (cadar e) (car e)) a)))
+             ((eq (caar e) (quote lambda))
+              (eval (caddar e)
+                    (append. (pair (cadar e) (evlis (cdr e) a))
+                             a))))))
+
+(def evcon (lambda (c a)
+             (cond ((eval (caar c) a)
+                    (eval (cadar c) a))
+                   (t (evcon (cdr c) a)))))
+
+(def evlis (lambda (m a)
+             (cond ((null m) (quote ()))
+                   (t (cons (eval  (car m) a)
+                            (evlis (cdr m) a))))))
